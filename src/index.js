f04f8186732b95f0843a1b0fb3b21802c6a40cfe
Asif Saif Ali <asif263848@gmail.com>
2023-05-05 22:32:28 +0530

    added dotenv file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-    
+    const express = require('express');
+    const { PORT } = require('./config/configServer');
+
+
+    const Setupserver=async()=>{
+
+        const app=express();
+
+        app.listen(PORT,()=>{
+            console.log(`Server is running at port ${PORT}`);
+        });
+    }
+
+    Setupserver();
